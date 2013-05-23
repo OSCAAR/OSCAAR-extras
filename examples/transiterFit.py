2c299de51fea24302112f2521769a373eeb6a4f7
Nolan <nmatthews@ubuntu.(none)>
2013-05-22 23:50:11 -0400

    Tweaked inital cond., updated correct inputs for transiter functions

--- examples/transiterFit.py
+++ examples/transiterFit.py
@@ -10,15 +10,17 @@ NormFlux=sampleData.lightCurve
 times=sampleData.times
 
 #Orbital Parameters for Observed Exoplanet
-Period = 2.2047 #days
-Rp = 0.077
-aRstar = 4.0
-inc = 83.0
+Period = 1.580 #days
+Rp = 0.11
+aRstar = 14.0
+inc = 89.0
 dt = times[2]-times[1]
-
 ecc = 0.0
 arg_per = 0.0
 
+#Running the fit 
 fit,success=transiter.fittransit(NormFlux,Rp,aRstar,inc,dt,Period)
+
+#Pulling output parameters from model parameters
 Flux,Rp,aRstarFIT,incFIT,aRstar,midtrantime=transiter.output_params(times,NormFlux,fit,success,Period,ecc,arg_per)
 
