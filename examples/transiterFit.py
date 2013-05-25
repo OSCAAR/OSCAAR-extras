44506e915c4c72d101b3d9d37e52fe0d0bf8e762
Nolan <nmatthews@ubuntu.(none)>
2013-05-24 20:59:29 -0400

    Included random dataset generator, fitting, and output with mean and stand. dev.

--- examples/transiterFit.py
+++ examples/transiterFit.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 import oscaar
 from oscaar import transiter
 from scipy import optimize
+import random
 
 sampleData = oscaar.load('sampleOutput/oscaarDataBase.pkl')
 
@@ -27,7 +28,7 @@ modelParams = [RpOverRs,aOverRs,period,inclination,gamma1,gamma2,eccentricity,lo
 #Duration of Transit
 durationObs = timeObs[np.size(timeObs)-1]-timeObs[0]
 
-#Running the fit, look at results 
+#Running the fit
 fit,success=optimize.curve_fit(oscaar.transitModel.occultquad,xdata=timeObs,
                                ydata=NormFlux,
                                p0=(RpOverRs,aOverRs,inclination,epoch),
@@ -35,6 +36,7 @@ fit,success=optimize.curve_fit(oscaar.transitModel.occultquad,xdata=timeObs,
                                xtol=2e-15,
                                ftol=2e-16)
 
+#Look at the results
 for i in range(0,np.size(fit)):
     print fit[i],np.sqrt(success[i][i])
 
@@ -44,8 +46,38 @@ plt.plot(timeObs,NormFlux,'o')
 plt.show()
 
 #Run Prayer-Bead or Random Markov Chain to estimate uncertainties.
+def shuffle(x):
+    x = list(x)
+    random.shuffle(x)
+    return x
 
+#Generate residuals
+modelOut  = oscaar.transitModel.occultquad(timeObs,fit[0],fit[1],fit[2],fit[3])
+residuals = NormFlux - modelOut
 
+RpFit,aRsFit,incFit,epochFit = fit[0],fit[1],fit[2],fit[3]
 
+#Generate random datasets based on residuals from inital fit. 
+n_sets = 1000
+Rp,aRs,inc,mid=[],[],[],[]
+MCset,randSet = [],[]
+for i in range(0,n_sets):
+    MCset = shuffle(residuals)
+    randSet = MCset + modelOut
+    fit,success=optimize.curve_fit(oscaar.transitModel.occultquad,xdata=timeObs,
+                               ydata=randSet,
+                               p0=(RpFit,aRsFit,incFit,epochFit),
+                               maxfev=10000000,
+                               xtol=2e-15,
+                               ftol=2e-16)
+    
+    #Save output parameters from fit
+    Rp.append(fit[0])
+    aRs.append(fit[1])
+    inc.append(fit[2])
+    mid.append(fit[3])
 
-
+print "Planetary to Stellar Radius: ",np.mean(Rp),np.std(Rp)
+print "Semi-major Axis to Stellar Radius: ",np.mean(aRs),np.std(aRs)
+print "Inclination of Orbit: ",np.mean(inc),np.std(inc)
+print "Mid-Transit Time [JD]: ",np.mean(mid),np.std(mid)
