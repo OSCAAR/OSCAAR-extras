9da48483ae9d00854474cbeb50f3857cce2f9b83
Nolan <nmatthews@ubuntu.(none)>
2013-06-03 14:57:30 -0400

    Included limb-darkening option in run_LMfit

--- examples/runtransiterModel.py
+++ examples/runtransiterModel.py
@@ -4,6 +4,7 @@ import oscaar
 from scipy import optimize
 from numpy.random import shuffle
 import transiterFit
+from oscaar.extras.knownSystemParameters import returnSystemParams
 
 #Load in the data from OSCAAR output. 
 #sampleData = oscaar.load('sampleOutput/oscaarDataBase.pkl')
@@ -11,63 +12,53 @@ import transiterFit
 #timeObs=sampleData.times
 #flux_error=sampleData.lightCurveError
 
-#Orbital Parameters for GJ1208
-#RpOverRs = 0.117		## R_p/R_s = ratio of planetary radius to stellar radius
-#aOverRs = 14.7                  ## a/R_s = ratio of semimajor axis to stellar radius
-#period = 1.58			## Period [days]
-#inclination = 89.0		## Inclination [degrees]
-#epoch = timeObs[np.size(timeObs)/2]+0.001 # Mid transit time [Julian date], guess is at middle of the dataset. 
-#gamma1 = 0.23			## Linear limb-darkening coefficient
-#gamma2 = 0.30			## Quadratic limb-darkening coefficient
-#eccentricity = 0.0		## Eccentricity
-#longPericenter = 0.0		## Longitude of pericenter
-
-#def getPeriod():
-#    return period
-
-#def getGam1():
-#    return gamma1
-
-#def getGam2():
-#    return gamma2
-
-#def getEcc():
-#    return eccentricity
-
-#def getArgPer():
-#    return longPericenter
-
-
-#Parameters for making fake data for a fake planet
+#Planetary Parameters to create fake data. 
 fk_RpRs=0.12
 fk_aRs=12.7
 fk_per=1.58
 fk_inc=89.5
 fk_t0=2454344.30867
 fk_gam1=0.23
-fk_gam2=0.30
+fk_gam2=0.50
 fk_ecc=0.0
 fk_argper=0.0
-stddev=0.001 #Standard Deviation of data. 
+stddev=0.0015 #Standard Deviation of data. 
 
 #Creating fake dataset, including flux, time, and uncertainty
 timeObs,NormFlux=transiterFit.fake_data(stddev,fk_RpRs,fk_aRs,fk_per,fk_inc,fk_t0,fk_gam1,fk_gam2,fk_ecc,fk_argper)
 flux_error=stddev*np.ones(np.size(timeObs))
 
-## Initial Guesses for Planetary Parameters  
-RpOverRs = 0.117
-aOverRs = 12.9
+#Initial Guess for Planetary Parameters by pulling them from the exoplanet website.
+#For this example we're using GJ 1214 b
+RpOverRs= 0.12
+aOverRs = 13.0
 period = 1.58
 inclination = 89.5
 epoch = 2454344.30867
 gamma1 = 0.23
 gamma2 = 0.30
-eccentricity = 0.0
 longPericenter = 0.0
 
+#Get parameters from exoplanet.org 
+planet = 'GJ 1214 b'
+#[p,ap,P,inc,eccentricity] = returnSystemParams.transiterParams(planet)
+
+#RpOverRs = returnSystemParams.exoplanetDB[planet]['PER']
+#aOverRs =  ap #returnSystemParams.exoplanetDB[planet]['PER']
+#period = P
+#inclination = inc
+#epoch = timeObs[np.size(timeObs)/2] #Guess at the middle of the dataset
+#gamma1 = 0.23 #For now have to look these up. 
+#gamma2 = 0.30
+#eccentricity = eccentricity
+#longPericenter = 0.0
+
 #Run the intial fit with input parameters stated above. 
-fit,success = transiterFit.run_LMfit(timeObs,NormFlux,flux_error,RpOverRs,aOverRs,inclination,epoch,plotting=True)
+fit,success = transiterFit.run_LMfit(timeObs,NormFlux,flux_error,RpOverRs,aOverRs,inclination,epoch,
+                                     fitLimbDark='quadratic',
+                                     plotting=True)
 
 #Run MC fit to estimate uncertainty
-transiterFit.run_MCfit(1000,timeObs,NormFlux,flux_error,fit,success,plotting=True)
+n_iter = 800 #Number of random datasets to fit to. 
+transiterFit.run_MCfit(n_iter,timeObs,NormFlux,flux_error,fit,success,plotting=True)
 
